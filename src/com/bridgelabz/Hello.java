3f53d5f9986d5c11c28b67837f1a7a972ef4b728
Roshni <roshni.mali@bridgelabz.com>
2022-06-11 22:41:23 +0530

    Refactored: Print message

--- src/com/bridgelabz/Hello.java
+++ src/com/bridgelabz/Hello.java
@@ -3,5 +3,6 @@ package com.bridgelabz;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Bridgelabz");
+        System.out.println("My name is Roshni");
     }
 }
